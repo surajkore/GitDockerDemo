e714155085c696dcb15f85d600465adb9a3ac3b6
suraj <surajkore32@gmail.com>
2021-09-23 13:20:58 +0530

    initial commit

---
+++ src/main/java/com/cicd/jenkins/GitDockerDemo/GitDockerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.cicd.jenkins.GitDockerDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDockerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitDockerDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cicd/jenkins/GitDockerDemo/GitDockerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicd.jenkins.GitDockerDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDockerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
